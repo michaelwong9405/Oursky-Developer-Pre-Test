925822bf792b22a0b1069ef1231bf51fec65f893
michaelwong9405 <45203126+michaelwong9405@users.noreply.github.com>
2018-11-21 02:57:17 +0800

    Update answer3.java

--- answer3.java
+++ answer3.java
@@ -3,12 +3,28 @@ public class answer3{
      static int f(int n){
        if(n == 0){
         return 0;
-       } else if (n == 1){
+    } else if (n == 1){
         return 1;
-       }else {return f(n-1) + f(n-2);}
-     }
+    }else {return f(n-1) + f(n-2);}
 
+    }
+    
      public static void main(String []args){
-       System.out.println(f(2));
-     }
+
+    int number[] ={1,22,9};
+    
+    
+    int a=0;
+    int arrLength = number.length;
+    
+    while (a<arrLength){
+        int n=0;
+        while (f(n)<=number[a]){
+            n++;
+         }
+        System.out.println(f(n));
+
+    a++;
+    }
+    } 
 }
